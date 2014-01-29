9d034b864d845362ca21a98e5dfbc3be78a85a90
John Albietz <inthecloud247@gmail.com>
2014-01-28 19:14:47 -0800

    Update ssh-agent.sh

--- ssh-agent.sh
+++ ssh-agent.sh
@@ -1,1 +1,4 @@
-git clone https://github.com/ccontavalli/ssh-ident.git
+git clone https://github.com/ccontavalli/ssh-ident.git /opt/github/ssh-ident
+ln -s /opt/github/ssh-ident/ssh-ident /opt/ssh-ident
+echo "alias ssh='/opt/github/ssh-ident/ssh-ident'" >> ~/.bash_aliases
+#then logout/login and ssh-agent now works properly..
