5844f406d98a633b4baaf7d4353fc02937e85977
John Albietz <inthecloud247@gmail.com>
2014-01-28 19:15:27 -0800

    Update ssh-agent.sh

--- ssh-agent.sh
+++ ssh-agent.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+#run this as root..
+
 git clone https://github.com/ccontavalli/ssh-ident.git /opt/github/ssh-ident
 ln -s /opt/github/ssh-ident/ssh-ident /opt/ssh-ident
 echo "alias ssh='/opt/github/ssh-ident/ssh-ident'" >> ~/.bash_aliases
